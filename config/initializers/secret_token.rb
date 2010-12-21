cfa0af506e8895ddfc800735870ac359a8eeb6c7
Prakash <prakashkumar198@gmail.com>
2010-12-21 18:42:08 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RendezWho::Application.config.secret_token = '799d5591ff9853e3b0a83b704649c2861dc0cc78fcd54dc10e50e252b1262438ebc943beffad995c085e04e8535069163bd193bf6c18551606624258ca95c446'
